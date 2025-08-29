cb00bf2351b77c52e7f7da841ddaec42bd89e015
hozaif <04072212010@student.qau.edu.pk>
2025-08-29 23:10:40 +0500

    fixed the issue of weather data not being fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getCurrentWeather, getForecast } from "./services/weatherAPI";
+import { Search } from "lucide-react";
+
 function App() {
-  const [city, setCity] = useState("Islamabad");
+  const [city, setCity] = useState("London");
   const [loading, setLoading] = useState(false);
   const [weather, setWeather] = useState(null);
   const [forecast, setForcecast] = useState(null);
+  const [isCelsius, setIsCelsius] = useState(true);
+
+  //Fetching weather data
   const fetchWeatherData = async () => {
     try {
       setLoading(true);
       let current = await getCurrentWeather(city);
       let forecastData = await getForecast(city);
       setWeather(current);
-      setForcecast(forecastData.forecast.forecastDay);
+      setForcecast(forecastData.forecast.forecastday);
     } catch (error) {
       console.error("Error fetching weather:", error);
     } finally {
       setLoading(false);
     }
   };
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
+  function setFormat(c, f) {
+    return isCelsius ? `${c} °C` : `${f}°F`;
+  }
   return <></>;
 }
 
--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -2,13 +2,13 @@ import axios from "axios";
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 const BASE_URL = "https://api.weatherapi.com/v1";
 export const getCurrentWeather = async (city) => {
-  const response = await axios.get(`${BASE_URL}\current.json`, {
+  const response = await axios.get(`${BASE_URL}/current.json`, {
     params: { key: API_KEY, q: city },
   });
   return response.data;
 };
-export const getForecast = (city) => {
-  const response = axios.get(`${BASE_URL}/forecast.json`, {
+export const getForecast = async (city) => {
+  const response = await axios.get(`${BASE_URL}/forecast.json`, {
     params: { q: city, key: API_KEY, days: 5 },
   });
   return response.data;
