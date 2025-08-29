c50e1bb2f733dcc69cf0e0cbfe528cce730a2a4b
hozaif <04072212010@student.qau.edu.pk>
2025-08-29 21:22:31 +0500

    Implemented  weather data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { getCurrentWeather, getForecast } from "./services/weatherAPI";
 function App() {
+  const [city, setCity] = useState("Islamabad");
+  const [loading, setLoading] = useState(false);
+  const [weather, setWeather] = useState(null);
+  const [forecast, setForcecast] = useState(null);
+  const fetchWeatherData = async () => {
+    try {
+      setLoading(true);
+      let current = await getCurrentWeather(city);
+      let forecastData = await getForecast(city);
+      setWeather(current);
+      setForcecast(forecastData.forecast.forecastDay);
+    } catch (error) {
+      console.error("Error fetching weather:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return <></>;
 }
 
